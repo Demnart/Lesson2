94a8a3b9664428c36768fee8225fd46177986270
Genjo <insidia93@gmail.com>
2016-09-19 13:33:42 +0300

    ArrayList(How we should do!!!)

---
+++ src/ArrayList/Main2.java
@@ -0,0 +1,24 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Артем on 19.09.2016.
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new <Integer>ArrayList();
+        //Так правильно обьявлять коллекции,т.к. мы четко указываем тот тип с которым мы работаем
+
+        list.add(14);
+        list.add('%');
+        list.add("Hello");
+
+        // На примере видно что после обьявления коллекции типа int(Integer  - класс обертка над int)
+        // Мы не можем ввести ни символьное значение
+        // Мы не можем ввести ни строку
+
+
+
+    }
+}
